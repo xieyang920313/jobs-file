702522e804eed83b51a3f9cbff7cf7e3ff1492de
xieyang920313 <owen.xie.chongqing@aliyun.com>
2023-01-29 16:23:32 +0800

    edit

---
+++ src/components/Other.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="other">
+    <CommonHead :title="'其他链接'"></CommonHead>
+    <div class="other-detail">
+      <div class="links" v-for="(link, index) in links" :key="index">
+        <el-icon><Share /></el-icon>
+        <div>
+          <label class="link-label">{{ link.name }}:</label>
+          <span class="link-url">{{ link.url }}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      links: [
+        {
+          name: "GitHub",
+          url: "https://github.com/xieyang920313",
+        },
+      ],
+    };
+  },
+  methods: {},
+  components: {},
+};
+</script>
+
+<style scoped >
+.links {
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+}
+.link-label {
+  margin: 0 20px 0 5px;
+  font-size: 14px;
+  font-weight: 700;
+}
+.link-url {
+  color: blue;
+  text-decoration: underline;
+  cursor: pointer;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,7 @@
     <WorkInfo></WorkInfo>
     <SchoolInfo></SchoolInfo>
     <SelfReview></SelfReview>
+    <Other></Other>
   </div>
 </template>
 
@@ -12,9 +13,16 @@ import PersonInfo from "@/components/PersonInfo.vue";
 import WorkInfo from "@/components/WorkInfo.vue";
 import SchoolInfo from "@/components/SchoolInfo.vue";
 import SelfReview from "@/components/SelfReview.vue";
+import Other from "@/components/Other.vue";
 export default {
   name: "HomeView",
-  components: { PersonInfo, WorkInfo, SchoolInfo, SelfReview },
+  components: {
+    PersonInfo,
+    WorkInfo,
+    SchoolInfo,
+    SelfReview,
+    Other,
+  },
 };
 </script>
 <style scoped>
